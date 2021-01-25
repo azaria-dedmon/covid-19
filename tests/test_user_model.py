ba945651f696d4965388c523bb66bea02ffefa67
Azaria Dedmon <azariadedmon@Azarias-MBP.attlocal.net>
2021-01-25 09:51:21 -0500

    covid project database structure

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,13 +1,10 @@
 """User model tests."""
-
 from unittest import TestCase
 from sqlalchemy import exc
 
-from models import db, User, Review
+from models import db, User
 
 from app import app
-
-
 
 
 class UserModelTestCase(TestCase):
@@ -21,9 +18,15 @@ class UserModelTestCase(TestCase):
         db.drop_all()
         db.create_all()
 
-        u1 = User(firstname="test", lastname="user", username="testuser123", 
-        email="test@test.com", password="password", image=None, 
-        state="California", vax_date=None, covid_status=None)
+        u1 = User(firstname="test", 
+                    lastname="user",
+                    username="testuser123", 
+                    email="test@test.com", 
+                    password="password", 
+                    image=None, 
+                    state="California", 
+                    vax_date=None, 
+                    covid_status=None)
         uid1 = 1111
         u1.id = uid1
 
@@ -58,3 +61,38 @@ class UserModelTestCase(TestCase):
         db.session.commit()
 
         self.assertEqual(len(u.review), 0)
+
+    def test_signup(self):
+        """Are users able to sign up?"""
+
+        user = User.signup('test', 
+                            'dummy', 
+                            'test123', 
+                            'dummytest@test.com', 
+                            'password', 
+                            None,
+                            "Texas", 
+                            None, 
+                            None)
+        user_id = 99999
+        user.id = user_id
+        db.session.commit()
+
+        u_test = User.query.get(user.id)
+        self.assertEqual(u_test.username, 'test123')
+
+
+    def test_invalid_email_signup(self):
+        user = User.signup('test', 
+                            'dummy',
+                            'test123',
+                            None, 
+                            'password', 
+                            None,
+                            "Texas",
+                            None,
+                            None)
+        user_id = 991010
+        user.id = user_id
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
