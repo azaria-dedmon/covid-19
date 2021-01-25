8c2688dab7bc789131cf377442e4858542fc8af5
Azaria Dedmon <azariadedmon@Azarias-MBP.attlocal.net>
2021-01-25 10:35:28 -0500

    adding server file 'app.py'

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+import os
+
+from flask import Flask, render_template, request, session, redirect, flash, g
+from flask_sqlalchemy import SQLAlchemy
+from models import connect_db, db, User, Review, testing_states
+from forms import LoginUser, RegisterUser, Reviews
+import requests
+
+CURR_USER_KEY = "curr_user"
+API_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
+
+key = os.environ.get('key')
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///covid'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = "secretkey"
+
+connect_db(app)
+db.create_all()
 
 
 
